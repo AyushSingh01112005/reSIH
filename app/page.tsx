7ffbe844848ae1c520aac7444b709ddf79b97e96
Ayush Singh 01 <singhayush.01112005@gmail.com>
2026-08-21 22:53:11 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const Page = () => {
+  return (
+    <div className="min-h-screen bg-gray-950">
+      <header className="flex justify-center items-center py-8">
+        <h1 className="text-4xl font-bold text-white">
+          SIH Project
+        </h1>
+      </header>
+    </div>
+  )
+}
+
+export default Page
