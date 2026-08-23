5db22dc617aab7a45cfda369ebc68ea4c7099a69
Ayush Singh 01 <singhayush.01112005@gmail.com>
2026-08-23 17:50:32 +0530

    Initial commit last

--- models/SensorReading.js
+++ models/SensorReading.js
@@ -4,28 +4,23 @@ const SensorReadingSchema = new mongoose.Schema(
   {
     deviceId: {
       type: String,
-      required: true,
       index: true,
     },
 
     temperature: {
       type: Number,
-      required: true,
     },
 
     humidity: {
       type: Number,
-      required: true,
     },
 
     gas: {
       type: Number,
-      required: true,
     },
 
     co2: {
       type: Number,
-      required: true,
     },
 
     motion: {
