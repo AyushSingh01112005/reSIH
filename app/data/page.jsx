12ebf726b16ce1f14456e94713a5bef1acf819d3
Ayush Singh 01 <singhayush.01112005@gmail.com>
2026-08-29 00:05:10 +0530

    changed dashboard/page.jsx to aryan wala .env vhange by ayush 2

---
+++ app/data/page.jsx
@@ -0,0 +1,7 @@
+export default function DataPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">HI</h1>
+    </main>
+  );
+}
